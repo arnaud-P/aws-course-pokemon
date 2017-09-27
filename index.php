5393002474f22f61a02533864a79668254c762ae
Arnaud PENIN <penin.arnaud@gmail.com>
2017-09-27 15:14:30 +0200

    Connexion à l'API Pokémon et restitution du pokédex.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$pokemons = file_get_contents('http://pokeapi.co/api/v2/pokedex/1/');
+$pokemons = json_decode($pokemons, true);
+
+$pokemon_entries = $pokemons['pokemon_entries'];
+
+foreach($pokemon_entries as $pokemon_entrie) {
+	echo $pokemon_entrie['name'] . '</br>';
+}
+
